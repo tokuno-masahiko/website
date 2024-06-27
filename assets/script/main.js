be25bd5245837183acaa7b7e60bb4ea3d91789ba
tokuno-masahiko <tokuno5956@gmail.com>
2024-06-27 21:57:48 +0900

    よくある質問ページ作成を着手したコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,3 +8,17 @@ $(function () {
         }
     });
 });
+
+$(function(){
+    $('.js_container').click(function(){
+        $(this).find('.js_txt').slideToggle();
+        $(this).find('.js_txt').css('display', 'flex');
+        if ($(this).find('.js_up').css('display') == 'none') {
+            $(this).find('.js_up').css('display', 'block');
+            $(this).find('.js_down').css('display', 'none');
+        } else {
+            $(this).find('.js_up').css('display', 'none');
+            $(this).find('.js_down').css('display', 'block');
+        }
+    });
+});
