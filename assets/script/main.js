aaf92aa7489d2f5e6f214f4d91aae677ec0d050f
tokuno-masahiko <tokuno5956@gmail.com>
2024-07-20 11:58:48 +0900

    初稿後の修正　完成

--- assets/script/main.js
+++ assets/script/main.js
@@ -12,14 +12,8 @@ $(function () {
 $(function(){
     $('.faq__card').click(function(){
         $(this).find('.faq__answer').slideToggle();
-        $(this).find('.faq__answer').css('display', 'flex');
-        if ($(this).find('.faq__icon1').css('display') == 'none') {
-            $(this).find('.faq__icon1').css('display', 'block');
-            $(this).find('.faq__icon2').css('display', 'none');
-        } else {
-            $(this).find('.faq__icon1').css('display', 'none');
-            $(this).find('.faq__icon2').css('display', 'block');
-        }
+        $(this).find('.faq__icon1').toggle();
+        $(this).find('.faq__icon2').toggle();
     });
 });
 
